bdfd4fbf7960b4e1fd50a443721b5dc30dd26607
charvitha2001 <105193684+charvitha2001@users.noreply.github.com>
2022-06-04 12:39:24 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "sample shell script that will return the difference between two numbers"
+
+Sum=$(($First_Number+$Second_Number))
+
+echo "$Sum"
+echo "Script is executed "
+
+echo "text comit 9"
